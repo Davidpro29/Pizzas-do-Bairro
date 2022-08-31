9ad4752598aa9338005e8896fce31bcd2f1b9afd
David Robison <silva.estudo@hotmail.com>
2022-08-31 15:25:37 -0300

    modal e funcionalidades

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
+let modalQt = 1;
+let cart = [];
+let modalKey = 0;
 
 // funções
 const c = (e)=> document.querySelector(e)
 const cAll = (e)=> document.querySelectorAll(e)
 
+ // informações 
 pizzaJson.map((item, index) =>{
     let pizzaItem = c('.models .pizza-item').cloneNode(true);
-
-    // informações 
 
     pizzaItem.setAttribute('data-key', index);
     pizzaItem.querySelector('.pizza-item--img img').src = item.img;
@@ -18,12 +20,22 @@ pizzaJson.map((item, index) =>{
         e.preventDefault();
 
         let key = e.target.closest('.pizza-item').getAttribute('data-key')
+        modalQt = 1;
+        modalKey = key;
 
         c('.pizzaBig img').src = pizzaJson[key].img;
         c('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
         c('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
         c('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`;
-
+        c('.pizzaInfo--size.selected').classList.remove('selected');
+        cAll('.pizzaInfo--size').forEach((size, sizeIndex)=>{
+            if(sizeIndex == 2){
+                size.classList.add('selected');
+            }
+            size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
+        })
+        c('.pizzaInfo--qt').innerHTML = modalQt;
+        
         c('.pizzaWindowArea').style.opacity = 0;
         c('.pizzaWindowArea').style.display = 'flex';
         setInterval(() => {
@@ -32,4 +44,51 @@ pizzaJson.map((item, index) =>{
     })
 
     c('.pizza-area').append(pizzaItem)
+})
+
+// eventos no modal
+
+function closeModal(){
+    c('.pizzaWindowArea').style.opacity = 0;
+    setTimeout(()=>{
+        c('.pizzaWindowArea').style.display = 'none';
+    }, 500)
+}
+
+cAll('.pizzaInfo--cancelMobileButton, .pizzaInfo--cancelButton').forEach((item)=>{
+    item.addEventListener('click', closeModal);
+})
+
+// aumentar e diminuir a qtd
+c('.pizzaInfo--qtmais').addEventListener('click', ()=>{
+    modalQt++;
+    c('.pizzaInfo--qt').innerHTML = modalQt;
+})
+
+c('.pizzaInfo--qtmenos').addEventListener('click', ()=>{
+    if(modalQt > 1){
+        modalQt--;
+        c('.pizzaInfo--qt').innerHTML = modalQt;
+    }    
+})
+
+// tamanhos das pizzas
+cAll('.pizzaInfo--size').forEach((size, sizeIndex)=>{
+    size.addEventListener('click', (e)=>{
+        c('.pizzaInfo--size.selected').classList.remove('selected');
+        size.classList.add('selected');
+    })
+})
+
+// adicionar ao carrinho / qual pizza, tamanho, quantas
+c('.pizzaInfo--addButton').addEventListener('click', ()=>{
+    let size = parseInt(c('.pizzaInfo--addButton.selected').getAttribute('data-key'));
+
+    cart.push({
+        id: pizzaJson[modalKey].id,
+        size, 
+        qt:modalQt
+    })
+
+    closeModal();
 })
